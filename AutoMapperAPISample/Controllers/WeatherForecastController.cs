1c136d271a59cc3f6dec61ab0544eb192834a643
Emman <nocilla.emman@gmail.com>
2023-09-12 20:28:21 +0200

    indentation

--- AutoMapperAPISample/Controllers/WeatherForecastController.cs
+++ AutoMapperAPISample/Controllers/WeatherForecastController.cs
@@ -13,9 +13,9 @@ namespace AutoMapperAPISample.Controllers
         private readonly IMapper _mapper;
 
         private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 

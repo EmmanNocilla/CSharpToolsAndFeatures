6af73a72cd29d7406f012773a136ac9608b3d40a
Emman <nocilla.emman@gmail.com>
2023-09-12 20:27:46 +0200

    Automapper

---
+++ AutoMapperAPISample/Profiles/ProductProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using AutoMapperAPISample.DTO;
+using AutoMapperAPISample.Entity;
+
+namespace AutoMapperAPISample.Profiles
+{
+    public class ProductProfile: Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDTO>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price));
+        }
+
+    }
+}
